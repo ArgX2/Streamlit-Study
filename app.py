6363c2d057efc2717ade124a558878d27d3af909
ArgX2 <121152042+ArgX2@users.noreply.github.com>
2022-12-21 15:52:03 +0900

    Add files via upload

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import pandas as pd
+from PIL import Image
+image = Image.open('alpago.jpg')
 
+st.title("Henlo")
 view = [100,150,30]
 st.write("# Youtube View")
 st.write("## raw")
@@ -8,3 +11,4 @@ st.write("## Bar Chart")
 st.bar_chart(view)
 sview = pd.Series(view)
 sview
+st.image(image, caption="사진 테스트.")
