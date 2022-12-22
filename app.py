a61146250514b0c4959b0ce1da97ade0e5782602
ArgX2 <121152042+ArgX2@users.noreply.github.com>
2022-12-22 15:59:20 +0900

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ image = Image.open('alpago.jpg')
 st.title("Henlo")
 st.image(image, caption="사진 테스트.")
 
-st.checkbox("이름 수정하기", key="disabled")
+key = st.checkbox("이름 수정하기")
 
 Num = st.slider('인원 입력 후 버튼 클릭.', 0, 50, 25)
 st.write("현재 설정 된 인원 수:",Num)
@@ -18,8 +18,8 @@ NameList = list(range(Num))
 
 st.write('')
 
-with 1:
-    disabled=st.session_state.disabled
+if key:
+    
     Stack = 0
 
     Name = st.text_input('여기에 이름 입력',Stack)
